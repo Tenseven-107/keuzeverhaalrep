f4995aeb3abac4e3ed17656e70427c44e2d61e6f
Tenseven-107 <78176691+Tenseven-107@users.noreply.github.com>
2022-10-13 14:03:57 +0200

    Made the inventory system existent of a list instead

--- keuzeverhaal.py
+++ keuzeverhaal.py
@@ -10,18 +10,15 @@ use: list = ["Gebruik", "gebruik", "G", "g"]
 move: list = ["Move", "move", "M", "m"]
 
 # Items
-has_screwdriver: bool = False
-has_medkit: bool = False
-has_food: bool = False
-has_warm_clothes: bool = False
+inventory: list = []
+
+# Misc
+player_name: str
 
 
 # Pick items
 def pick_items():
-    global has_screwdriver
-    global has_medkit
-    global has_food
-    global has_warm_clothes
+    global inventory
 
     print("Keuzes: Schroevendraaier, EHBO doos, 4 blikken voedsel, Warme kleren")
 
@@ -33,16 +30,16 @@ def pick_items():
         match picked_item:
             case "Schroevendraaier":
                 print("Je hebt nu een schroevendraaier.")
-                has_screwdriver = True
+                inventory.append("Schroevendraaier")
             case "EHBO doos":
                 print("Je hebt nu een EHBO doos.")
-                has_medkit = True
+                inventory.append("EHBO doos")
             case "4 blikken voedsel":
                 print("Je hebt nu 4 blikken eten.")
-                has_food = True
+                inventory.append("4 blikken voedsel")
             case "Warme kleren":
                 print("Je hebt nu warme kleding")
-                has_warm_clothes = True
+                inventory.append("Warme kleren")
         
         if picked_item not in items:
             print("Sorry, dat ligt er niet.")
@@ -55,30 +52,38 @@ def pick_items():
 
 
 
+# Start
+def prologue():
+    global player_name
 
-# Get player name
-name = input("Wat is je naam? ")
-time.sleep(0.5)
+    # Get player name
+    name = input("Wat is je naam? ")
+    player_name = name
+    time.sleep(0.5)
 
-# Intro
-print("Het is 2010. In het land Oerand heerst een hevige oorlog om olie. Het is een nutteloze oorlog die vele levens claimt en gezinnen uit elkaar trekt. ")
-print("Mensen proberen te vluchter maar worden getroffen door mensen handel ")
-print("en de wereld van drugs...")
+    # Intro
+    print("Het is 2010. In het land Oerand heerst een hevige oorlog om olie. Het is een nutteloze oorlog die vele levens claimt en gezinnen uit elkaar trekt. ")
+    print("Mensen proberen te vluchter maar worden getroffen door mensen handel ")
+    print("en de wereld van drugs...")
 
-time.sleep(3)
+    time.sleep(3)
 
-print("Jij bent een tiener, genaamd", str(name) + ".")
-print("Je bent van plan om te ontsnappen uit Oerand en asiel te zoeken in Heeland, een Westers land met genoeg kansen. Je zit op dit moment thuis, ")
-print("je ouders zijn een tijd geleden spoorloos verdwenen. Er valt een lichte straal van elegant licht naar binnen en er ")
-print("dwarrelen allemaal stof deeltjes door de lucht. Er liggen een aantal objecten in het huis, ")
-print("je besluit er een aantal mee te nemen in je rugzak voor je gaat.")
+    print("Jij bent een tiener, genaamd", str(name) + ".")
+    print("Je bent van plan om te ontsnappen uit Oerand en asiel te zoeken in Heeland, een Westers land met genoeg kansen. Je zit op dit moment thuis, ")
+    print("je ouders zijn een tijd geleden spoorloos verdwenen. Er valt een lichte straal van elegant licht naar binnen en er ")
+    print("dwarrelen allemaal stof deeltjes door de lucht. Er liggen een aantal objecten in het huis. ")
+    print("Je besluit er een aantal mee te nemen in je rugzak voor je gaat.")
 
-time.sleep(5)
+    time.sleep(5)
 
-pick_items()
+    pick_items()
 
-print("Vannacht gaat er een groepje uit je dorp via de jungle naar het strand om de boot te nemen om te ontsnappen. Je besluit om mee te gaan vanavond.")
+    print("Vannacht gaat er een groepje uit je dorp via de jungle naar het strand om de boot te nemen om te ontsnappen. Je besluit om mee te gaan vanavond.")
 
-time.sleep(2)
+    time.sleep(2)
 
-print("Het is nu 21:00 en het is tijd om te gaan.")
+    print("Het is nu 21:00 en het is tijd om te gaan.")
+
+
+
+prologue() # Start game
